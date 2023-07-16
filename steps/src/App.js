3296636e35ce3aa5f47954ce9b31d35103655f6f
BradleyMurrell <95315836+BradleyMurrell@users.noreply.github.com>
2023-07-16 22:19:33 +0000

    Add text component in App js

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,1 +1,3 @@
-
+export default function App() {
+  return <div>Hello React</div>;
+}
--- steps/src/index.js
+++ steps/src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-function App() {
-  return <h1>Hello React!</h1>;
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
